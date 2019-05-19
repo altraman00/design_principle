2e4fb83d22f4b0771d4b0dff882aea4bf1449c33
xiekun <123456>
2019-05-19 16:38:06 +0800

    first commit

---
+++ src/main/java/com/mdl/design/principle/DesignPrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.mdl.design.principle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPrincipleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPrincipleApplication.class, args);
+    }
+
+}
