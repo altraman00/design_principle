e88b8a8f0edf61cf600970d2fba3815652ebc515
xiekun <123456>
2019-05-25 17:32:10 +0800

    排除掉不需要提交的文件

---
+++ src/main/java/com/mdl/design/principle/pattern/creational/simplefactory/Video.java
@@ -0,0 +1,17 @@
+package com.mdl.design.principle.pattern.creational.simplefactory;
+
+/**
+ * @Project : design_principle
+ * @Package Name : com.mdl.design.principle.pattern.creational.simplefactory
+ * @Description : TODO
+ * @Author : admin
+ * @Create Date : 2019年05月19日 16:51
+ * @ModificationHistory Who   When     What
+ * ------------    --------------    ---------------------------------
+ */
+public class Video {
+
+
+     //test
+
+}
